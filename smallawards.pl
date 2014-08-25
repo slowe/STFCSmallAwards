0efe82d47c28e980ace8dd7bb1e5534dffaecd26
Stuart <gravatar@strudel.org.uk>
2014-08-26 00:21:45 +0100

    Add populations of regions and URLs for sources

--- smallawards.pl
+++ smallawards.pl
@@ -4,40 +4,68 @@ open(FILE,"smallawards.txt");
 @lines = <FILE>;
 close(FILE);
 
-$round = 0;
-$sources = 0;
-$json = "{\n\t\"awards\": [\n";
+$inrounds = 0;
+$insources = 0;
 
 $i = 0;	# The number of awards
+$j = 0; # The number of regions
+
+$json = "{\n";
+$json_awards = "\t\"awards\": [\n";
+$json_regions = "\t\"regions\": [\n";
 
 foreach $line (@lines){
 
+	$line =~ s/\"/\'/g;	# replace all quotation marks
+	$line =~ s/[\n\r]//g;	# remove new lines
+	
+
 	if($line =~ /^Sources/){
-		$sources = 1;
-		$round = 0;
-		next;
-	}
-	if($line =~ /^Round/){
-		$round = 1;
-		$sources = 0;
+		$insources = 1;
+		$inrounds = 0;
+		$inregions = 0;
 		next;
 	}
 
-	if($sources==1){
+	if($line =~ /^Round/){
+		$inrounds = 1;
+		$insources = 0;
+		$inregions = 0;
+		next;
+	}
+
+	if($line =~ /^Regions/){
+		$inrounds = 0;
+		$insources = 0;
+		$inregions = 1;
+		next;
+	}
+
+	if($insources==1){
 		if($line =~ /^\d\d\d\d/){
 			($r,$url) = split(/\t/,$line);
 			$source{$r} = $url;
 		}
 	}
 
-	if($round==1){
+	if($inregions==1){
+		if($line =~ /^[a-zA-Z]/){
+		
+			($r,$pop) = split(/\t/,$line);
+			$j++;
+			# Construct the JSON output
+			if($j > 1){
+				$json_regions .= ",\n";
+			}
+			$json_regions .= "\t\t{ \"region\": \"$r\", \"population\": $pop }";
+		}	
+	}
+
+	if($inrounds==1){
 		if($line =~ /^\d\d\d\d/){
 
 			$i++;
 
-			$line =~ s/\"/\'/g;	# replace all quotation marks
-			$line =~ s/[\n\r]//g;	# remove new lines
-			
 			($r,$amount,$title,$org,$location,$region) = split(/\t/,$line);
 
 			# Find the year from the round
@@ -46,14 +74,16 @@ foreach $line (@lines){
 
 			# Construct the JSON output
 			if($i > 1){
-				$json .= ",\n";
+				$json_awards .= ",\n";
 			}
-			$json .= "\t\t{ \"year\": $year, \"round\": \"$r\", \"amount\": $amount, \"title\": \"$title\", \"org\": \"$org\", \"location\": \"$location\", \"region\": \"$region\" }";
-
+			$json_awards .= "\t\t{ \"year\": $year, \"round\": \"$r\", \"amount\": $amount, \"title\": \"$title\", \"org\": \"$org\", \"location\": \"$location\", \"region\": \"$region\", \"source\": \"$source{$r}\" }";
 		}
 	}
 }
-$json .= "\n\t]\n}";
+$json_awards .= "\n\t]";
+$json_regions .= "\n\t]";
+$json .= $json_regions.",\n".$json_awards;
+$json .= "\n}";
 
 open(FILE,">","smallawards.json");
 print FILE $json;
